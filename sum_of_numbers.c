4e8741ebb02523048e27fb356dc2c05d2e1a5577
ENG19CS0082-DeekshaYD <75901288+ENG19CS0082-DeekshaYD@users.noreply.github.com>
2021-02-04 14:40:52 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,1 +1,14 @@
-//Write a program to find the sum of n different numbers using 4 functions
+//Write a program to add two numbers
+
+#include <stdio.h>
+
+int main()
+{  
+    float a,b;
+    printf("Enter Number:");
+    scanf("%f",&a);
+    printf("Enter Number:");
+    scanf("%f",&b);
+    printf("Sum of %f and %f is : %f",a,b,a+b);
+    return 0;
+}
