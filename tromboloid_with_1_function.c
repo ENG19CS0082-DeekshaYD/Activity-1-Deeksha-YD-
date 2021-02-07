e3eb0001c81cf839143f0796c7abb4b212d35ecd
ENG19CS0082-DeekshaYD <75901288+ENG19CS0082-DeekshaYD@users.noreply.github.com>
2021-02-07 22:53:26 +0530

    write commit

--- tromboloid_with_1_function.c
+++ tromboloid_with_1_function.c
@@ -8,7 +8,7 @@ int main()
     float volume=0;
     printf("Enter the values of h,b,d:\n");
     scanf("%f%f%f",&h,&b,&d);
-    volume=(float)1/(float)3*((h*b*d)+(d/b);
+    volume=(float)1/(float)3*((h*b*d)+(d/b));
     printf("Volume of tromboloid is :%f",volume);
     return 0;
 }
