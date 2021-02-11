7efd0061add3ac259853cb7e03e648388e1b81be
ENG19CS0082-DeekshaYD <75901288+ENG19CS0082-DeekshaYD@users.noreply.github.com>
2021-02-11 13:46:30 +0530

    initial commit

--- add_with_1_function.c
+++ add_with_1_function.c
@@ -1,1 +1,44 @@
-//Write a program to add two user input numbers using one function.
+//Write a program to add n different numbers
+
+#include<stdio.h>
+int input_n()
+{
+    int a;
+    printf("Enter numbers of elements to be added:");
+    scanf("%d",&a);
+    return a;
+}
+void input_array(int n,float a[n])
+{
+    for(int i=0;i<n;i++)
+    {
+    printf("Enter the %d th element of an array:",i);
+    scanf("%f",&a[i]);
+    }
+}
+float array_sum(int n,float a[n])
+{
+    float sum=0;
+    for(int i=0;i<n;i++)
+    {
+        sum += a[i];
+    }
+    return sum;
+}
+void show_results(int n, float a[n], float sum)
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+        printf("%f=%.2f",a[i],sum);
+    }
+}
+int main()
+{
+    int n=input_n();
+    float a[n];
+    input_array(n,a);
+    float sum=array_sum(n,a);
+    show_results(n,a,sum);
+    return 0;
+}
