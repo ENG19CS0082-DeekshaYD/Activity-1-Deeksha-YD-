5334914b2781dec2e1cdad1c667efe60d6dab631
ENG19CS0082-DeekshaYD <75901288+ENG19CS0082-DeekshaYD@users.noreply.github.com>
2021-02-11 11:53:23 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -3,7 +3,7 @@
 #include<stdio.h>
 #include<math.h>
 float input(char b1[])
-{   
+{
     float a;
     printf("Enter the %s co-ordinate:",b1);
     scanf("%f", &a);
@@ -20,12 +20,12 @@ void output(float a1,float a2, float b1, float b2,float result)
 }
 int main()
 {
-   float x1,x2,y1,y2,D;
-   x1=input("x1");
-   y1=input("y1");
-   x2=input("x2");
-   y2=input("y2");
-   D=compute_distance(x1,x2,y1,y2);
-   output(x1,y1,x2,y2,D);
-   return 0;
+    float x1,x2,y1,y2,D;
+    x1=input("x1");
+    y1=input("y1");
+    x2=input("x2");
+    y2=input("y2");
+    D=compute_distance(x1,x2,y1,y2);
+    output(x1,y1,x2,y2,D);
+    return 0;
 }
