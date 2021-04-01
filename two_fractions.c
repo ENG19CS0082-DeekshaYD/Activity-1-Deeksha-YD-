76177923f060f98d1a29f0a897f60d0573bcbc1a
ENG19CS0082-DeekshaYD <75901288+ENG19CS0082-DeekshaYD@users.noreply.github.com>
2021-04-01 18:39:37 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -3,17 +3,15 @@
 #include<stdio.h>
 struct fraction
 {
-	int Num,Deno;
+    int Num,Deno;
 };
 typedef struct fraction Fract;
-Fract input(int n)
-{
-	Fract f;
-	printf("Enter the value of numerator for fraction %d :",n);
-	scanf("%d",&f.Num);
-	printf("Enter the value of denominator for fraction %d :",n);
-	scanf("%d",&f.Deno);
-	return f;
+Fract input(char a[])
+{    
+    Fract temp;
+    printf("enter the value of %s:",a);
+    scanf("%d%d",&temp.nr,&temp.dr);
+    return temp;
 }
 int find_gcd(int a, int b)
 {
@@ -28,29 +26,29 @@ int find_gcd(int a, int b)
 }
 Fract simplify(Fract sum)
 {
-	int gcd=find_gcd(sum.Num,sum.Deno);
-	sum.Num=sum.Num/gcd;
-	sum.Deno=sum.Deno/gcd;
-	return sum;
+    int gcd=find_gcd(sum.Num,sum.Deno);
+    sum.Num=sum.Num/gcd;
+    sum.Deno=sum.Deno/gcd;
+    return sum;
 }
 Fract add_fraction(Fract f1, Fract f2)
-{
+{   
     Fract sum;
     sum.Num=(f1.Num*f2.Deno)+(f2.Num*f1.Deno);
     sum.Deno=f1.Deno*f2.Deno;
-	sum=simplify(sum);
+    sum=simplify(sum);
     return sum;
 }
 void display_sum(Fract f1, Fract f2, Fract sum)
 {
-	printf("The sum of %d/%d and %d/%d is: %d/%d.\n",f1.Num,f1.Deno,f2.Num,f2.Deno,sum.Num,sum.Deno);
+    printf("The sum of %d/%d and %d/%d is: %d/%d.\n",f1.Num,f1.Deno,f2.Num,f2.Deno,sum.Num,sum.Deno);
 }
 int main()
 {
-	Fract f1,f2,sum;
-	f1=input(1);
-	f2=input(2);
-	sum=add_fraction(f1,f2);
-	display_sum(f1,f2,sum);
-	return 0;
+   Fract f1,f2,sum;
+   f1=input("a/b");
+   f2=input("c/d");
+   sum=add_fraction(f1,f2);
+   display_sum(f1,f2,sum);
+   return 0;
 }
